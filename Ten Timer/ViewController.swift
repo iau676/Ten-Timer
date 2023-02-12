324eb9a211f44e4e5a53671f73bd59a684b70de5
iau676 <ibrahimuysal676@gmail.com>
2023-02-13 01:14:58 +0300

    Initial Commit

---
+++ Ten Timer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ten Timer
+//
+//  Created by ibrahim uysal on 13.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
